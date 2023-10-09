270f5998d5d1d9c0a9dac2d724567d98d6b474f2
John O'Connell <oconnell.j@northeastern.edu>
2023-10-09 14:35:45 -0400

    start of multiprocessing assignmnet

---
+++ multiprocessing/genRandWord-helper.h
@@ -0,0 +1,11 @@
+/*
+* genRandWord-helper.h / Random Number Generation
+*
+* John O'Connell / CS5600 / Northeastern University
+* Fall 2023 / Oct 8, 2023
+*
+*/
+
+void print_help();
+
+const char* genRandWord();
