44e86e30d7c5ace94437678dd80a511cadb37088
John O'Connell <oconnell.j@northeastern.edu>
2023-10-10 17:30:42 -0400

    added missing helper files

---
+++ multithreading/multithreading-helper.c
@@ -0,0 +1,82 @@
+/*
+* multithreading-helper.c / Multithreading
+*
+* John O'Connell / CS5600 / Northeastern University
+* Fall 2023 / Oct 8, 2023
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "polybius.h"
+#include "multithreading-helper.h"
+
+char pbSquare[SQUARE_SIZE][SQUARE_SIZE] = {
+    {':', ';', '<', '=', '>', '?'},
+    {'@', 'A', 'B', 'C', 'D', 'E'},
+    {'F', 'G', 'H', 'I', 'J', 'K'},
+    {'L', 'M', 'N', 'O', 'P', 'Q'},
+    {'R', 'S', 'T', 'U', 'V', 'W'},
+    {'X', 'Y', 'Z', '[', '\\', ']'}
+};
+
+/*
+ * Prints the help message for the multiprocessing program
+ * 
+ */
+void print_help_multi(){
+	printf("multiprocessing. (2023 Oct 8)\n\n");
+	printf("Usage: multiprocess <file_name>\n");
+}
+
+/*
+ * Invokes the cipher program used to write encoded output to a file
+ *
+ * @param data: file that the function will read from
+ * 
+ */
+void* invokeCipher(void* data) {
+    char* inputFileName = (char*)data;
+    char outputFileName[256];
+
+    // Generate a unique output file for the ciphered batch
+    time_t current_time = time(NULL);
+    snprintf(outputFileName, sizeof(outputFileName), "output/output_%ld.txt", current_time);
+
+    FILE* fp;
+    // open the input file in read mode
+	fp = fopen(inputFileName, "r");
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    FILE* fp2;
+    // open the output file in write mode
+    fp2 = fopen(outputFileName, "w");
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    char line[256];
+
+    // Read each word from input file, encode it and write it to output file
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        line[strcspn(line, "\n")] = '\0'; // Remove the newline character
+        char* word = pbEncode(line, pbSquare);
+        fprintf(fp2, "%s", word);
+        fprintf(fp2, "\n");
+        free(word);
+    }
+
+    fclose(fp);
+    fclose(fp2);
+    remove(inputFileName);
+
+    return NULL;
+}
---
+++ multithreading/multithreading-helper.h
@@ -0,0 +1,11 @@
+/*
+* multithreading-helper.h / Multithreading
+*
+* John O'Connell / CS5600 / Northeastern University
+* Fall 2023 / Oct 8, 2023
+*
+*/
+
+void print_help_multi();
+
+void* invokeCipher(void* data);
