9003243d054b53d58e00c7e38eb0284bf3ff7ea1
John O'Connell <oconnell.j@northeastern.edu>
2023-12-05 22:55:01 -0500

    added multi-threading test

--- practicum2/unitTest.sh
+++ practicum2/unitTest.sh
@@ -3,14 +3,14 @@
 # Test performing WRITE operation
 echo "----------Test WRITE Command----------"
 echo "Writing foo.txt :"
-./rfs WRITE foo.txt
+./rfs WRITE testFiles/foo.txt foo.txt
 if [ -e rfsys/foo.txt ]; then
         echo "File foo.txt exists on remote server"
 fi
 
 echo
 echo "Writing foo.txt to folder new/ :"
-./rfs WRITE foo.txt new/foo.txt
+./rfs WRITE testFiles/foo.txt new/foo.txt
 if [ -e rfsys/new/foo.txt ]; then
         echo "File foo.txt exists in folder new/ on remote server"
 fi
@@ -35,15 +35,16 @@ echo
 # Test performing versioned GET operation
 echo "----------Test GET Command - VERSIONED----------"
 echo "Currently foo.txt contains the following :"
-cat foo.txt
+cat testFiles/foo.txt
 echo
 echo
 echo "We will write this new version to foo.txt :"
-cat newfoo.txt
+cat testFiles/newfoo.txt
 echo
 echo
 echo "Writing new version of foo.txt :"
-./rfs WRITE newfoo.txt foo.txt
+./rfs WRITE testFiles/newfoo.txt foo.txt
+echo
 echo "Here is the latest version using the GET command :"
 ./rfs GET foo.txt getVersFoo.txt
 cat getVersFoo.txt
@@ -60,6 +61,31 @@ echo "----------Test LS Command----------"
 ./rfs LS foo.txt
 echo
 ./rfs LS new/foo.txt
+echo
+
+# Test multi-threading functionality
+echo "----------Test Multi-threading Functionality----------"
+echo "Writing 5 files concurrently :"
+# Loop to simulate multi-threading
+for ((i=0; i<5; i++)); do
+    # Concatenate i with the filename
+    filename="testFiles/test${i}.txt"
+    # Run WRITE command
+    ./rfs WRITE "$filename" test.txt
+done
+sleep 1
+echo "Printing each versioned file to ensure all files written :"
+for ((i=0; i<4; i++)); do
+    # Concatenate i with the filename
+    filename="v${i}_test.txt"
+    echo "File: $filename"
+    # Run tasks in the background
+    cat rfsys/vers/$filename
+    echo
+done
+echo "File: test.txt"
+cat rfsys/test.txt
+echo
 echo
 
 # Test performing RM operation
